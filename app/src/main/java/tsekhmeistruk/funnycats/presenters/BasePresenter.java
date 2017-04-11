a616b3745dfd32221eaa13955f0ad038a4c3fc69
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-04-11 21:41:51 +0300

    Added BasePresenter class

---
+++ app/src/main/java/tsekhmeistruk/funnycats/presenters/BasePresenter.java
@@ -0,0 +1,34 @@
+package tsekhmeistruk.funnycats.presenters;
+
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+import tsekhmeistruk.funnycats.views.BaseView;
+
+/**
+ * Created by Roman Tsekhmeistruk on 11.04.2017.
+ */
+
+public class BasePresenter<T extends BaseView> {
+
+    private T view;
+
+    private CompositeSubscription compositeSubscription = new CompositeSubscription();
+
+    public T getView() {
+        return view;
+    }
+
+    public void setView(T view) {
+        this.view = view;
+    }
+
+    public void destroy() {
+        compositeSubscription.clear();
+    }
+
+    protected Subscription subscribe(Subscription subscription) {
+        compositeSubscription.add(subscription);
+        return subscription;
+    }
+
+}
