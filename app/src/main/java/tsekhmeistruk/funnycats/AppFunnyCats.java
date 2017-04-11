db7cbfdf4cbff3e9227c7c7a6a12dca220aaf4db
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-04-11 21:50:57 +0300

    Added application class

---
+++ app/src/main/java/tsekhmeistruk/funnycats/AppFunnyCats.java
@@ -0,0 +1,31 @@
+package tsekhmeistruk.funnycats;
+
+import android.support.multidex.MultiDexApplication;
+
+import tsekhmeistruk.funnycats.di.component.AppComponent;
+import tsekhmeistruk.funnycats.di.component.DaggerAppComponent;
+import tsekhmeistruk.funnycats.di.module.ApiModule;
+import tsekhmeistruk.funnycats.di.module.AppModule;
+
+/**
+ * Created by Roman Tsekhmeistruk on 11.04.2017.
+ */
+
+public class AppFunnyCats extends MultiDexApplication {
+
+    private AppComponent appComponent;
+
+    public AppFunnyCats() {
+        super();
+
+        appComponent = DaggerAppComponent.builder()
+                .apiModule(new ApiModule())
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent appComponent() {
+        return appComponent;
+    }
+
+}
