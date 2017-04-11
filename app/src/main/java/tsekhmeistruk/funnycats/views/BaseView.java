5607b829a8db3b421b95f0a02508edd441285a78
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-04-11 21:41:18 +0300

    Added BaseView interface

---
+++ app/src/main/java/tsekhmeistruk/funnycats/views/BaseView.java
@@ -0,0 +1,13 @@
+package tsekhmeistruk.funnycats.views;
+
+import android.content.Context;
+
+/**
+ * Created by Roman Tsekhmeistruk on 11.04.2017.
+ */
+
+public interface BaseView {
+
+    Context getContext();
+
+}
