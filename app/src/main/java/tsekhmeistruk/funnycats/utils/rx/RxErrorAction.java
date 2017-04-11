344ac2d5929bde1007aa98a6a4dcce2ce4a9a57b
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-04-11 21:44:45 +0300

    Added rx utils

---
+++ app/src/main/java/tsekhmeistruk/funnycats/utils/rx/RxErrorAction.java
@@ -0,0 +1,29 @@
+package tsekhmeistruk.funnycats.utils.rx;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import rx.functions.Action1;
+import tsekhmeistruk.funnycats.R;
+
+/**
+ * Created by Roman Tsekhmeistruk on 27.02.2017.
+ */
+
+public class RxErrorAction implements Action1<Throwable> {
+
+    private final Context context;
+
+    public RxErrorAction(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void call(Throwable throwable) {
+        throwable.printStackTrace();
+
+        Toast.makeText(context, context.getString(R.string.request_execution_has_failed),
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
