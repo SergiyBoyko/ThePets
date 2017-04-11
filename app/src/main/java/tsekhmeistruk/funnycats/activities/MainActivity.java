85175c17e8bad4ca2561be5143cd7fb1e651500d
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-04-11 18:00:30 +0300

    Main activity class moved to another package

--- app/src/main/java/tsekhmeistruk/funnycats/activities/MainActivity.java
+++ app/src/main/java/tsekhmeistruk/funnycats/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package tsekhmeistruk.funnycats;
+package tsekhmeistruk.funnycats.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -7,11 +7,11 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import tsekhmeistruk.funnycats.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
