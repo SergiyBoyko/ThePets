62d5fd1a46c01c21af59d0a91fb973260c034abd
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-04-11 18:07:23 +0300

    Added Constants class

---
+++ app/src/main/java/tsekhmeistruk/funnycats/Constants.java
@@ -0,0 +1,12 @@
+package tsekhmeistruk.funnycats;
+
+/**
+ * Created by Roman Tsekhmeistruk on 11.04.2017.
+ */
+
+public class Constants {
+
+    private final String THE_CAT_API = "http://thecatapi.com/";
+    private final String THE_CAT_API_KEY = "MTQ5ODk3";
+
+}
