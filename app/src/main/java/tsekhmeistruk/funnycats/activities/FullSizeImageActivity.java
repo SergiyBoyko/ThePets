5e3a379d59c6d30039c721cf8e756dc9358f71ca
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-08-06 13:10:44 +0300

    Added activity for displaying full size image

---
+++ app/src/main/java/tsekhmeistruk/funnycats/activities/FullSizeImageActivity.java
@@ -0,0 +1,15 @@
+package tsekhmeistruk.funnycats.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tsekhmeistruk.funnycats.R;
+
+public class FullSizeImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_size_image);
+    }
+}
