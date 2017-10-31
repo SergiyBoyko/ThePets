0750217fd7586dde2636693788d91ab0df9cfdfe
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-10-31 15:15:20 +0200

    cats activity attached to button

--- app/src/main/java/tsekhmeistruk/funnycats/activities/StartActivity.java
+++ app/src/main/java/tsekhmeistruk/funnycats/activities/StartActivity.java
@@ -1,12 +1,16 @@
 package tsekhmeistruk.funnycats.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 import tsekhmeistruk.funnycats.R;
+import tsekhmeistruk.funnycats.activities.cats_activities.CatsActivity;
 import tsekhmeistruk.funnycats.models.cats.entities.Image;
 
 public class StartActivity extends AppCompatActivity {
@@ -15,6 +19,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
 
+    public void onCatButtonPressed(View view) {
+        startActivity(new Intent(StartActivity.this, CatsActivity.class));
+    }
+
+    public void onDogButtonPressed(View view) {
+        Toast.makeText(this, "dog pressed", Toast.LENGTH_SHORT).show();
+
+        // TODO: 31.10.2017 implement and invoke
     }
 }
